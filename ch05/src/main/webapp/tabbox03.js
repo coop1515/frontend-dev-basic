9ad64aa04bf5de136ae66bc4cec791153ef06ada
김현석 <gustjr324@gmail.com>
2022-07-05 17:25:01 +0900

    ..

--- ch05/src/main/webapp/tabbox03.js
+++ ch05/src/main/webapp/tabbox03.js
@@ -1,39 +1,40 @@
-// DOM Event Model1 Level0: basic: HTML Element의 Event Attribute에 
-// Event Handler(함수)를 설정하는 방식 
-// JavaScript Code(DOM API)로만 작성하기. 
+// jQuery로 작성하기 
+
 var tabBox = {
-	/*_liSelected: null,*/
 	init: function() {
-		/*window.onload = this._onWindowLoad.bind(this);*/
-		window.addEventListener('load', this._onWindowLoad.bind(this));
+		/*window.addEventListener('load', this._onWindowLoad.bind(this));*/
+		$(this._onWindowLoad.bind(this)
+		);
 	},
 	_onTabClicked: function() {
-	// console.log(this);
-	/*if(tabBox._liSelected != null){
-		tabBox._liSelected.className = '';
-	}*/	
-	
-	// unselected
-	var lisSelected = document.getElementsByClassName('selected');
-	lisSelected.length == 1 && (lisSelected[0].className = '');
+	/*var lisSelected = document.getElementsByClassName('selected');*/
+	var $lisSelected = $('.selected');
+	$lisSelected.length == 1 && ($lisSelected[0] = ''); //(lisSelected[0].className = '')
+	console.log($lisSelected[0]);
 	
 	// select
 	this.className = 'selected';
-	// tabBox._liSelected = this;
 	
 	},
 	_onWindowLoad: function(){
-		/*	document.getElementById("tab01").onclick = this._onTabClicked;
-			document.getElementById("tab02").onclick = this._onTabClicked;
-			document.getElementById("tab03").onclick = this._onTabClicked;
-			document.getElementById("tab04").onclick = this._onTabClicked;
-			document.getElementById("tab05").onclick = this._onTabClicked;*/
-		var divTabBox = document.getElementsByClassName("tab-box")[0];
+		/*var divTabBox = document.getElementsByClassName("tab-box")[0];*/
+		var divTabBox = $('.tab-box')[0];
 		var ul = divTabBox.childNodes[1];
-		var liTabs = ul.getElementsByTagName("li");
+		/*var liTabs = ul.getElementsByTagName("li");*/
+		var liTabs = $("li");
 		
 		for(var i = 0; i < liTabs.length; i++){
 			liTabs[i].addEventListener("click", this._onTabClicked)
+			
+			
 		}
 	}
 }
+/*
+$(function(){
+	$("ul li")
+	.click(function(){
+		$("ul li").removeClass("selected");
+		$(this).addClass("selected");
+	})
+});*/
