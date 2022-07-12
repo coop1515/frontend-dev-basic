9735d0722618512ec7b11c0f29083f484aabd4ae
김현석 <gustjr324@gmail.com>
2022-07-12 14:34:36 +0900

    es6#4

---
+++ javascript-practices/es6/ex09.01.mjs
@@ -0,0 +1,12 @@
+/**
+ * unamed export with default
+ */
+
+export default function(a, b){
+    return a+ b;
+}
+
+// 오류: 이름 없이 export를 하기 때문에 하나만 default로 export할 수 있다.
+// export default function(a, b, c){
+//     return a+ b + c;
+// }
